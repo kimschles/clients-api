6e7fcfe40898d262365b906097a279eaef5f7631
coloradokim <schlesinger.kim@gmail.com>
2017-06-22 19:34:07 -0600

    remove unused function

--- api/clients.js
+++ api/clients.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/:id', isValidId, (req, res, next) => {
+router.get('/:id', (req, res, next) => {
   queries.getOne(req.params.id).then(client => {
     if(client) {
       res.json(client);
