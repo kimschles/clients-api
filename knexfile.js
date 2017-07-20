c940d3c367379fe4e7c6ab7b69f53354854569cd
coloradokim <schlesinger.kim@gmail.com>
2017-07-20 15:27:39 -0600

    remove production enviornment

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,6 @@
 module.exports = {
-    development: {
-      client: 'pg',
-      connection: 'postgres://localhost/clients'
-    },
-    production: {
-      client: 'pg',
-      connection: process.env.DATABASE_URL
-    }
-  };
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/clients'
+  }
+};
