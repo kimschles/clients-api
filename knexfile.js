d8128567a01da2833a7d122bd0896b7879c58391
coloradokim <schlesinger.kim@gmail.com>
2017-07-20 16:02:12 -0600

    update connection

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,12 @@
+require('dotenv').config()
+
 module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/clients'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
